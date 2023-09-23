5ca43980770ac7979fcb5ba613ac754203e18bcc
Felipe Saavedra <fisaavedrae@gmail.com>
2023-09-23 17:34:28 -0300

    start

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,35 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="container">
+        <div class="layout">
+          <div class="titulo">
+            <h1>Crea una cuenta</h1>
+          </div>
+          //Cristian
+          <div class="social">
+            <div class="icono">
+              <i class="fa-brands fa-square-facebook"></i>
+              <i class="fa-brands fa-square-github"></i>
+              <i class="fa-brands fa-linkedin"></i>
+            </div>
+            <div class="bajadaIcono">O usa tu email para registrarte</div>
+          </div>
+          //Cristian
+          <div class="formulario">
+            //Felipe
+            <input type="text" id="nombre" placeholder="Nombre" />
+            <input type="email" id="email" placeholder="tuemail@ejemplo.com" />
+            <input type="password" id="password" placeholder="Contraseña" />
+            <input type="password" id="password2" placeholder="Confirma tu contraseña" />
+            <button class="registro">Registrarse</button>
+            //Felipe
+            //Conjunto
+            <div class="mensajeError">Completa todos los campos!</div>
+            //Conjunto
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
