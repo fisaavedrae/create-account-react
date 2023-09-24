34210aba5c0fc5fdb42df22fc14359322baf1ae1
Felipe Saavedra <fisaavedrae@gmail.com>
2023-09-24 17:03:10 -0300

    validaciones ok

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,8 +1,10 @@
-const Alert = () => {
+
+/*const Alert = () => {
     return (
-        <div className="alert alert-primary" role="alert">Completa todos los campos!</div>
+        
     )
 
 }
 
 export default Alert;
+*/
