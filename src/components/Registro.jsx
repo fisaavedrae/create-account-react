8efa3ada2efe595c6f0a84ce31c9ce5992c92b87
Felipe Saavedra <fisaavedrae@gmail.com>
2023-09-24 17:03:58 -0300

    registro

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -13,16 +13,18 @@ function Registro() {
             <div className="container">
                 <div className="card mb-md-5">
                     <Titulo titulo="Crear una cuenta" />
-                    {/* Cristian */}
+
                     <div className="social">
                         <div className="icono">
+                            {/* Cristian */}
                             <div className="socialButton"><i className="fa-brands fa-facebook"></i></div>
                             <div className="socialButton"><i className="fa-brands fa-github"></i></div>
                             <div className="socialButton"><i className="fa-brands fa-linkedin-in"></i></div>
+                            {/* Cristian */}
                         </div>
                         <div className="card-subtitle mb-2 text-body-secondary">O usa tu email para registrarte</div>
                     </div>
-                    {/* Cristian */}
+
                     <div className="formulario">
                         {/* Felipe */}
                         <Formulario />
